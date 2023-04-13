43d97b5ac7671e360c8897241e602abeb2341406
Jhoem Software <www.jhonlive90@gmail.com>
2023-04-12 23:41:37 -0500

    Inicio implementación de App Front - HomeComponent - Steady, Ready, Go project

--- SteadyReadyGo/public/src/App.vue
+++ SteadyReadyGo/public/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container index">
+    <div class="container">
         <nav class="navbar navbar-expand-lg">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-5">
                     <li class="nav-item">
                         <router-link to="/" class="text-uppercase">inicio</router-link>
                     </li>
--- SteadyReadyGo/public/src/components/HomeComponent.vue
+++ SteadyReadyGo/public/src/components/HomeComponent.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-        <div class="row justify-content-center">
-                <div class="col-12 row justify-content-center mb-3">
-                    <div class="col-12 col-sm-2 align-self-center">
-                        <img src="./../assets/logo.png" alt="Logo VueJS">
-                    </div>
-                    <div class="col-12 col-sm-2 align-self-center">
+    <div class="home">
+        <div class="row justify-content-around">
+                <div class="col-12 row justify-content-center mb-5">
+                    <div class="col-12 col-sm-3 align-self-center">
                         <img src="./../assets/nodejs.png" alt="Logo NodeJS">
+                    </div>
+                    <div class="col-12 col-sm-3 align-self-center">
+                        <img src="./../assets/js.png" alt="Logo NodeJS">
+                    </div>
+                    <div class="col-12 col-sm-3 align-self-center">
+                        <img src="./../assets/logo.png" alt="Logo VueJS">
                     </div>
                 </div>
                 <div class="col-9">
@@ -14,15 +17,11 @@
                     <h4>NodeJS from Hero to SuperHero</h4>
                 </div>
             </div>
-        <div class="container" style="text-align: left;">
-            <h1>Página de inicio</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, culpa. Nostrum dignissimos earum iusto aut nihil tempora fugiat voluptatum suscipit sapiente. Veritatis beatae aliquam vel saepe corrupti sapiente ut architecto?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, culpa. Nostrum dignissimos earum iusto aut nihil tempora fugiat voluptatum suscipit sapiente. Veritatis beatae aliquam vel saepe corrupti sapiente ut architecto?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, culpa. Nostrum dignissimos earum iusto aut nihil tempora fugiat voluptatum suscipit sapiente. Veritatis beatae aliquam vel saepe corrupti sapiente ut architecto?</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, culpa. Nostrum dignissimos earum iusto aut nihil tempora fugiat voluptatum suscipit sapiente. Veritatis beatae aliquam vel saepe corrupti sapiente ut architecto?</p>
+        <div class="container mt-5">
+            <h1>Steady, Ready, Go!</h1>
+            <p>
+                API Rest full con Node JS, Express, simulación de Database con FS y aplicación Front-end en Vue JS y Axios
+            </p>
         </div>
     </div>
 </template>
-<style>
-
-</style>
