9a8130eb5bb8de31fe9e471362a825f73cb4c01a
Jhoem Software <www.jhonlive90@gmail.com>
2023-04-13 21:59:57 -0500

    Implementación de Método Post en Axios - Steady, Ready,Go

--- SteadyReadyGo/public/src/components/ContentComponent.vue
+++ SteadyReadyGo/public/src/components/ContentComponent.vue
@@ -1,9 +1,57 @@
 <template>
     <div class="container">
-        <button type="button" class="btn btn-success">
+        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#modalAdd">
             <i class="fa-solid fa-plus"></i>
         </button>
-        <table class="table table-dark table-hover mt-3">
+
+        <!-- Modal Add -->
+        <div class="modal fade" id="modalAdd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Registro de Productos</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Form -->
+                        <form @submit.prevent="save">
+                            <div class="form-group mb-3">
+                                <label class="form-label">Nombre de Producto</label>
+                                <input autofocus required tabindex="1" type="text" class="form-control"
+                                    v-model="product.name" />
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="form-label">Referencia de Producto</label>
+                                <input required tabindex="2" type="text" class="form-control" v-model="product.reference" />
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="form-label">Precio de Producto</label>
+                                <input required tabindex="3" type="text" class="form-control" v-model="product.price" />
+                            </div>
+                            <div class="form-group mb-3 row">
+                                <div class="col-6">
+                                    <label class="form-label">Cilindraje Total de Motor</label>
+                                    <input required tabindex="4" type="text" class="form-control"
+                                        v-model="product.cylinder" />
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-label">Cantidad existencias</label>
+                                    <input required tabindex="5" type="text" class="form-control" v-model="product.stock" />
+                                </div>
+                            </div>
+                            <div class="form-group mb-3">
+                                <button type="submit" class="btn btn-info" tabindex="6">
+                                    <i class="fa-solid fa-floppy-disk"></i>
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <table class="table table-dark table-hover mt-5">
             <thead>
                 <tr>
                     <th scope="col">Código</th>
@@ -26,7 +74,7 @@
                     <td>$ {{ data.price }}</td>
                     <td>{{ data.stock }}</td>
                     <td>
-                        <button type="button" class="btn btn-primary">
+                        <button type="button" class="btn btn-primary" @click="ProductLoad(data.id)">
                             <i class="fa-solid fa-eye"></i>
                         </button>
                     </td>
@@ -53,12 +101,21 @@ export default {
     data() {
         return {
             result: {},
+            product: {
+                name: "",
+                reference: "",
+                price: "",
+                cylinder: "",
+                stock: "",
+            },
             page: "http://localhost:8088/api/v1/products/",
         };
     },
+
     created() {
         this.ProductsLoad();
     },
+
     methods: {
         async ProductsLoad() {
             await axios.get(this.page)
@@ -66,34 +123,40 @@ export default {
                 .catch(({ err }) => console.log(err))
         },
 
-        /* save() {
+        async ProductLoad(info) {
+            await axios.get(this.page + info)
+                .then(({ data }) => this.result = data.product)
+                .catch(({ err }) => console.log(err))
+        },
+
+        save() {
             this.ProductSave();
         },
-        ProductSave() {
-            axios
-                .post(this.page, this.product)
-                .then(({ data }) => {
-                    console.log(this.product);
-                    console.log(data);
-                    Swal.fire({
-                        position: 'top-end',
-                        icon: 'success',
-                        title: 'Your work has been saved',
-                        showConfirmButton: false,
-                        timer: 1500
-                    })
-                    this.ProductsLoad();
+
+        async ProductSave() {
+            await axios.post(this.page, this.product)
+                .then((data) => {
+                    const { success } = data.data;
+                    setTimeout(() => {
+                        this.ProductsLoad();
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: success,
+                            showConfirmButton: false,
+                            timer: 1000
+                        });
+                    }, 800);
                 })
-                .catch(({ data }) => {
-                    console.log(data);
+                .catch(({ response }) => {
+                    const { error } = response.data;
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
-                        text: 'Something went wrong!',
-                        footer: '<a href="">Why do I have this issue?</a>'
+                        text: error
                     })
                 });
-        }, */
+        },
 
         deleted(info) {
             Swal.fire({
@@ -110,7 +173,7 @@ export default {
                 }
             })
         },
-        
+
         async ProductsDelete(product) {
             const url = `${this.page}${product}`;
 
