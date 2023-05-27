38c1f9b83c7025db8402803b23f9cccdd769ff71
Jhoem Software <www.jhonlive90@gmail.com>
2023-05-27 14:56:22 -0500

    Resolucion de conflictos en Rutas

--- Clase5/src/routes/index.js
+++ Clase5/src/routes/index.js
@@ -5,13 +5,8 @@ const router = Router();
 
 router
     .use('/products', require('./products'))
-<<<<<<< HEAD
     .use('/health', require('./health'))
     .get('/login', (_, res) => res.sendFile(path.resolve('Clase5/src/views/login.html')) )
     .get('/playground', (_, res) => res.sendFile(path.resolve('Clase5/src/views/playground.html')) );
-=======
-    .use('/users', require('./user'))
-    .use('/health', require('./health'));
->>>>>>> 04c1d28b248ec420c23c6665aa55cde25452d199
 
 module.exports = router;
